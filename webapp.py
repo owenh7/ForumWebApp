91c51698e0a48e5502eb93f1b20fd4b3e58ffb78
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-07 09:26:45 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder='templates')
 def render_main():
     print("RunningMain")
     return render_template('page1.html')
-  @app.route("/p1")
+@app.route("/p1")
 def render_first():
     return render_template('page1.html')
 @app.route("/p2")
