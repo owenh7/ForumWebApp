426ee0bcd4f1fc3b7d9d030b1dd9ba58bbe92017
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-11 09:59:30 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,7 +15,7 @@ def render_main():
 @app.route('/startOver')
 def startOver():
     session.clear() #clears variable values and creates a new session
-    return redirect(render_template('page1.html')) 
+    return redirect('/page1')
 @app.route('/page1',methods=['GET','POST'])
 def renderPage1():
     return render_template('page1.html')
