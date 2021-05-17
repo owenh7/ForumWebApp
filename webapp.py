62b2b49b66f5116b849c50c8a043d145758e95eb
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-17 09:36:20 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -60,7 +60,11 @@ def startOver():
     return redirect('/page1')
 @app.route('/page1',methods=['GET','POST'])
 def renderPage1():
-    return render_template('page1.html')
+    if 'user_data' in session:
+        user_data_pprint = pprint.pformat(session['user_data'])#format the user data nicely
+    else:
+        user_data_pprint = '';
+    return render_template('page1.html',dump_user_data=user_data_pprint)
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
     return render_template('page2.html')
