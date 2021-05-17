1987886def95f9b1c4f89fe7dec72369166b9cf4
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-17 09:26:19 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -53,7 +53,7 @@ def authorized():
             session.clear()
             print(inst)
             message='Unable to login, please try again.  '
-    return render_template('page2.html')
+    return render_template('page1.html')
 @app.route('/startOver')
 def startOver():
     session.clear() 
