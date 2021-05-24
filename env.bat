c32a8e8939ce0c63e451bb5ec9cec874815ceffc
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-05-24 09:17:06 -0700

    Update env.bat

--- env.bat
+++ env.bat
@@ -1,4 +1,2 @@
 set MONGO_CONNECTION_STRING=mongodb+srv://Doughbaby:0RsVaIPgDTMKzaH8@porkbun.kpkqv.mongodb.net/Porkbun?retryWrites=true
 set MONGO_DBNAME=testdb
-
-b7492d8e31b7d0c4faab7660d147541ae51f05fa
