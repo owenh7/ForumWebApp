718d22c2e56dba29e64e605e0ccbabf0d674b54b
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-06-01 20:42:02 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,9 +9,9 @@ def main():
  connection_string = os.environ["MONGO_CONNECTION_STRING"]
  db_name = os.environ["MONGO_DBNAME"]
 
-    client = pymongo.MongoClient(connection_string)
-    db = client[db_name]
-    collection = db['Test']
+ client = pymongo.MongoClient(connection_string)
+ db = client[db_name]
+ collection = db['Test']
 
 app.secret_key=os.environ["SECRET_KEY"];
 oauth = OAuth(app)
