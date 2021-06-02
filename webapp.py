2ba24217d64b8c5e10c5b11fb3705f98252c5ba6
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-06-02 12:26:33 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -55,7 +55,9 @@ def main():
      if resp is None:
         session.clear()
         message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
+        
         else:
+            
         try:
             session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
             session['user_data']=github.get('user').data
