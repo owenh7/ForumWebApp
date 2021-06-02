e5fef1aa911b3175c0da02d10cbe156f6b6912af
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-06-01 20:54:54 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,15 +4,13 @@ import os
 import pymongo
 
 def main():
-
- app = Flask(__name__, template_folder='templates')
- connection_string = os.environ["MONGO_CONNECTION_STRING"]
+connection_string = os.environ["MONGO_CONNECTION_STRING"]
  db_name = os.environ["MONGO_DBNAME"]
 
  client = pymongo.MongoClient(connection_string)
  db = client[db_name]
  collection = db['Test']
-
+ app = Flask(__name__, template_folder='templates')
 app.secret_key=os.environ["SECRET_KEY"];
 oauth = OAuth(app)
 oauth.init_app(app)
