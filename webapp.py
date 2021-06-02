5bc1230a505d6e6c1dfb49ef2f6cb9250c336341
BishopH7 <77747379+BishopH7@users.noreply.github.com>
2021-06-02 12:24:03 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -55,7 +55,7 @@ def main():
      if resp is None:
         session.clear()
         message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
-    else:
+        else:
         try:
             session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
             session['user_data']=github.get('user').data
